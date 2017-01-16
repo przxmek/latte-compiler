188b120caf15abcd0ae33667712bef878fd4dbe6
Przemysław Kuczyński <p.kuczynski@student.uw.edu.pl>
2017-01-16 17:36:21 +0100

    Implementing Environment methods

--- src/Environment.hs
+++ src/Environment.hs
@@ -4,20 +4,58 @@ import           AbsLatte
 import           Control.Monad.State
 import qualified Data.Map            as M
 
-type Store = M.Map Ident Type
 
 data ClassMember = Var Type | Method Type [Type]
-type ClassStore = M.Map Ident (M.Map Ident ClassMember)
 
+type Store = M.Map Ident Type
+type ClassStore = M.Map Ident (M.Map Ident ClassMember)
 type ErrorMsg = String
-type Environment = ([Store], ClassStore, ErrorMsg)
+
+type Environment = ([Store], [ClassStore], ErrorMsg)
+
 type EnvState a = State Environment a
 
+
+getStores :: Environment -> [Store]
+getStores env = let (s, _, _) = env in s
+
+getClassStores :: Environment -> [ClassStore]
+getClassStores env = let (_, c, _) = env in c
+
+getErrorMsg :: Environment -> ErrorMsg
+getErrorMsg env = let (_, _, e) = env in e
+
+
 newScope :: EnvState ()
-newScope = return () -- @TODO
+newScope = do
+  (s, c, e) <- get
+  put(M.empty : s, M.empty : c, e)
 
 exitScope :: EnvState ()
-exitScope = return () -- @TODO
+exitScope = do
+  (s, c, e) <- get
+  put(tail s, tail c, e)
 
 appendError :: EnvState ()
-appendError = return () -- @TODO
+appendError = return ()
+
+
+lookupVarType :: Ident -> EnvState Type
+lookupVarType ident = do
+  (stores, _, _) <- get
+  lookupStores ident stores where
+    lookupStores ident' stores' = case stores' of
+      [] -> do
+        appendError
+        return $ BaseTypeDef TVoid
+      s:ss -> case M.lookup ident' s of
+        Just v -> return v
+        Nothing -> lookupStores ident' ss
+
+newVar :: Ident -> Type -> EnvState ()
+newVar ident newType = do
+  (store:stores, c, e) <- get
+  case M.lookup ident store of
+    Just _ -> appendError
+    Nothing ->
+      put(M.insert ident newType store:stores, c, e)
